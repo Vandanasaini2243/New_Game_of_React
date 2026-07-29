8a2af504d65cbf7a2b000428ded653de23f53886
Vandanasaini2243 <sainivandana259@gmail.com>
2026-07-30 02:58:42 +0530

    delete Comments

--- Counter/src/App.jsx
+++ Counter/src/App.jsx
@@ -1,37 +1,3 @@
-// import React, { useState } from 'react'
-
-// const App = () => {
-//   const[counter,setCounter]= useState(0);
-
-// const handleIncrement =()=>{
-//   setCounter((prev) => prev + 1);
-// }
-
-// const handleDecrement =()=>{
-//   setCounter((prev)=> prev -1);
-// }
-
-// const handleReset =()=>{
-  
-//   setCounter(0);
-// }
-
-
-//   return (
-//     <div style={{backgroundColor:"yellow",width:"100vh",height:"100vh",padding:50}}>
-
-//       <div style={{padding:50,display:"flex",alignItems:"center",justifyContent:"center",flexDirection:"column",backgroundColor:"pink",borderRadius:10}}>
-
-//         <button onClick={handleDecrement}>-</button>
-//         <h1>{counter}</h1>
-//         <button onClick={handleIncrement}>+</button>
-//         <button onClick={handleReset}>Reset</button>
-//       </div>
-//     </div>
-//   )
-// }
-
-// export default App
 import React, { useState } from "react";
 import "./App.css";
 
