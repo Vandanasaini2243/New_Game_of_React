d5d07f73c179458a18147e0d1705d49f89b67737
Vandanasaini2243 <sainivandana259@gmail.com>
2026-07-30 18:41:26 +0530

    Generic File Box

--- Counter/src/App.jsx
+++ Counter/src/App.jsx
@@ -1,42 +1,12 @@
 import React, { useState } from "react";
-import "./App.css";
+import Box from "./components/box";
+// import "./App.css";
 
 const App = () => {
-  const [counter, setCounter] = useState(0);
-
-  const handleIncrement = () => {
-    setCounter((prev) => prev + 1);
-  };
-
-  const handleDecrement = () => {
-    setCounter((prev) => prev - 1);
-  };
-
-  const handleReset = () => {
-    setCounter(0);
-  };
 
   return (
     <div className="container">
-      <div className="card">
-        <h1>Counter App</h1>
-
-        <h2 className="counter">{counter}</h2>
-
-        <div className="button-group">
-          <button className="btn decrease" onClick={handleDecrement}>
-            -
-          </button>
-
-          <button className="btn increase" onClick={handleIncrement}>
-            +
-          </button>
-        </div>
-
-        <button className="reset-btn" onClick={handleReset}>
-          Reset Counter
-        </button>
-      </div>
+    <Box/>
     </div>
   );
 };
